1c3ee4c79dc28f7d0f1bee5e1d94dd611a9cd59f
Steve Marshall <stevesonoa@gmail.com>
2021-11-03 13:47:10 -0700

    Added handCheck utility for all players. Manually tested only.

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const handCheck_test_1 = require("./utils/handCheck.test");
+const run = () => {
+    // testWithSomePlayers();
+    // testWithManyPlayers();
+    // testWithTooManyPlayers();
+    (0, handCheck_test_1.testRoyalFlush)();
+};
+run();
+//# sourceMappingURL=index.js.map
